423b1feea6c40bad480c385c4d0e9df4424f84be
Lalibela-Dev <abeventss123@gmail.com>
2022-09-20 01:20:21 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,20 @@
 #include "main.h"
 /**
- *reset_to_98 - check the code
- *@n: character to be checked
  *Return: Always 0.
  */
-void reset_to_98(int *n)
-{
-*n = 98;
+int main() {
+int a;
+int b;
+a = 98;
+b = 42;
+printf("a=%d, b=%d\n", a, b);
+swap_int(&a, &b);
+printf("a=%d, b=%d\n", a, b);  
+return 0;
+}
+void swap_int(int *x, int *y){
+int temp;
+temp = *x;
+*x = *y;
+*y = temp;
 }
