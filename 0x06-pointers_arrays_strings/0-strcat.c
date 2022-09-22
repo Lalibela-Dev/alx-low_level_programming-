e02257ea672fb304c25b7362bfd3948108c11af9
Lalibela-Dev <abeventss123@gmail.com>
2022-09-22 08:24:18 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,23 @@
 #include "main.h"
-/**
- * *_strncat - concatenates n bytes of two strings
- * @dest: pointer destination
- * @src: pointer source
- * @n: number of bytes
- * Return: void
+/**                                              
+ * *_strncat - concatenates n bytes of two strings  
+ * @dest: pointer destination                        
+ * @src: pointer source                             
+ * @n: number of bytes                              
+ * @Return: void                                     
  */
-char *_strncat(char *dest, char *src, int n)
+ char *_strcat(char dest[], char src[])       
 {
-int dest_len, i;
-for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[dest_len + i] = src[i];
-/*should end with a end of string char*/
-dest[dest_len + i] = '\0';
-return (dest);
+	 int length, j;
+	 length = 0;	
+	 while(dest[length] != '\0')
+	 {
+		 ++length;
+	 }
+	 for (j = 0; src[j] != '\0'; ++j, ++length)
+	 {
+		 dest[length] = src[j];
+	 }
+	 dest[length] = '\0';
+	 return dest;
 }
