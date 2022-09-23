fe3096e9b345f1cc779b5f18244acacc6de6abdc
Lalibela-Dev <abeventss123@gmail.com>
2022-09-23 02:25:05 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,9 +16,9 @@ p = &n;
 * - you are not allowed to use a
 * - you are not allowed to modify p
 * - only one statment
-* - you are not allowed to code anything else than this line oode
+* - you are not allowed to code anything else than this line of code
 */
-p[5] = 98;
+*(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
