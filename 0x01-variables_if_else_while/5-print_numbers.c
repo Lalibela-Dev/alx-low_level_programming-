2565c65c824026ee8669852c1bafdbc40cc74e1f
Lalibela-Dev <abeventss123@gmail.com>
2022-09-23 06:29:51 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+int d = 48;
+while (d < 58)
+{
+putchar(d);
+d++;
+}
+putchar('\n');
+return (0);
+}
