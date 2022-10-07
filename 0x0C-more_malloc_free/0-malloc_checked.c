d8386f01b5bd4135514c4deb8b952a3445f17f0a
Lalibela-Dev <abeventss123@gmail.com>
2022-10-07 13:00:47 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 /**
  *malloc_checked - allocates memory using malloc and exit if failed
  *@b: int
- Return: pointer to the array initialized or NULL
+ *Return: pointer to the array initialized or NULL
  */
 void *malloc_checked(unsigned int b)
 {
